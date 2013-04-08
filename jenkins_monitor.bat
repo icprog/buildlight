a953da15708c0fa78fdb668ee55cc720008a9a20
Lihao Yeong <lihao.yeong@iproperty.com>
2013-04-08 17:12:07 +0800

    Modify batch file: kill existing windows process before starting a new one

--- jenkins_monitor.bat
+++ jenkins_monitor.bat
@@ -1,3 +1,5 @@
-TASKKILL /F /IM "USBCMDAP.exe"
-# C:\wamp\bin\php\php5.3.10\php.exe ..\..\buildlights.php
-C:\wamp\bin\php\php5.3.10\php.exe ..\..\buildlights.php
+tasklist /FI "IMAGENAME eq USBCMDAP.exe" 2>NUL | find /I /N "USBCMDAP.exe">NUL
+if "%ERRORLEVEL%"=="0" taskkill /F /IM USBCMDAP.exe
+:: TASKKILL /F /IM "USBCMDAP.exe"
+C:\wamp\bin\php\php5.4.3\php.exe ..\..\buildlights.php
+Exit
